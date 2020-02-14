81b267eceb5972c655f6401b465134344306294d
n90540 <n90540@IS941190N90540P.scisb.isban.corp>
2020-02-14 13:02:46 +0100

    primer commit

---
+++ Prueba01/src/main/java/com/prueba01/ejemplo/HolaMundo.java
@@ -0,0 +1,11 @@
+package com.prueba01.ejemplo;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo");
+
+	}
+
+}
